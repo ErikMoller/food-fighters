c25bb4229e365ab60e394e530b165e871e10a635
Erik Möller <erik.moller@live.com>
2017-03-25 21:45:27 +0100

    start of sending data over rest to product-service

--- shell-app/src/main/java/com/foodfighters/shell/command/ProductServiceCommand.java
+++ shell-app/src/main/java/com/foodfighters/shell/command/ProductServiceCommand.java
@@ -1,9 +1,15 @@
 package com.foodfighters.shell.command;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by erimol on 2017-03-24.
@@ -11,11 +17,39 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductServiceCommand implements CommandMarker {
 
+    private Logger logger = Logger.getLogger(getClass());
 
-    @CliCommand(value = "erik", help = "Print a simple hello world message")
-    public String simple(
-            @CliOption(key = { "message" }, mandatory = true, help = "The hello world message") final String message,
-            @CliOption(key = { "location" }, mandatory = false, help = "Where you are saying hello", specifiedDefaultValue="At work") final String location) {
-        return "Message = [" + message + "] Location = [" + location + "]";
+
+    @Autowired private RestTemplate restTemplate;
+
+
+    @CliCommand(value = "product-service data", help = "Data action for production-service")
+    public String action(
+            @CliOption(key = { "action" }, mandatory = true, help = "Specify [init|clear] as action") final DataAction action) {
+
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String requestJson = "{\"name\":\"apple\",\"ingredients\":{\"ingredients\":[{\"name\":\"erik\"},{\"name\":\"majs\"}]},\"nutritionFacts\":null,\"externalLink\":null}";
+        HttpEntity<String> entity = new HttpEntity<String>(requestJson,headers);
+        restTemplate.postForObject("http://localhost:8080/v1/product",entity,Void.class);
+        switch (action) {
+            case clear:
+                logger.info("Clear products");
+                return "Cleared products";
+            case init:
+                logger.info("Init products");
+                return "Initialized products";
+            default:
+                return "error";
+        }
+    }
+
+    enum DataAction {
+        clear,
+        init
     }
 }
