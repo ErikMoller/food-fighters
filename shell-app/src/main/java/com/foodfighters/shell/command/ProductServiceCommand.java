417d8199e56e054a00025af9c48b9e16a7fd7b31
Erik Möller <erik.moller@live.com>
2017-03-26 16:42:07 +0200

    Add products for classpath json file

--- shell-app/src/main/java/com/foodfighters/shell/command/ProductServiceCommand.java
+++ shell-app/src/main/java/com/foodfighters/shell/command/ProductServiceCommand.java
@@ -1,6 +1,10 @@
 package com.foodfighters.shell.command;
 
 import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +14,11 @@ import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by erimol on 2017-03-24.
@@ -25,7 +34,7 @@ public class ProductServiceCommand implements CommandMarker {
 
     @CliCommand(value = "product-service data", help = "Data action for production-service")
     public String action(
-            @CliOption(key = { "action" }, mandatory = true, help = "Specify [init|clear] as action") final DataAction action) {
+            @CliOption(key = { "action" }, mandatory = true, help = "Specify [init|clear] as action") final DataAction action) throws Exception {
 
         switch (action) {
             case clear:
@@ -52,12 +61,27 @@ public class ProductServiceCommand implements CommandMarker {
         restTemplate.getForObject("http://localhost:8080/v1/product/clear",Void.class);
     }
 
-    private void addProducts() {
+    private void addProducts() throws IOException, ParseException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+        InputStream inputStream = ProductServiceCommand.class.getClassLoader().getResourceAsStream("testdata/proudcts.json");
+        System.out.println(inputStream);
 
-        String requestJson = "{\"name\":\"apple\",\"ingredients\":{\"ingredients\":[{\"name\":\"erik\"},{\"name\":\"majs\"}]},\"nutritionFacts\":null,\"externalLink\":null}";
-        HttpEntity<String> entity = new HttpEntity<String>(requestJson,headers);
-        restTemplate.postForObject("http://localhost:8080/v1/product",entity,Void.class);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(bufferedReader);
+
+        JSONArray products = (JSONArray) jsonObject.get("products");
+
+        logger.info("Start adding products");
+
+        int counter = 0;
+        for (Object product : products) {
+            String jsonString = ((JSONObject) product).toJSONString();
+            HttpEntity<Object> entity = new HttpEntity<>(jsonString,headers);
+            restTemplate.postForObject("http://localhost:8080/v1/product",entity,Void.class);
+            counter++;
+        }
+        logger.info(String.format("Added %s products",counter));
     }
 }
